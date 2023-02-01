8be46764b8310b1ec828ea53aa159e4c7465ee69
Ashlyn Carmosino <ashlyncarmosino@Ashlyns-Air.lan>
2023-02-01 14:50:54 -0500

    madlibs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// exercise one 
+{
+    Console.WriteLine("What is your name?");// output 
+    var name = Console.ReadLine(); // input
+
+    Console.WriteLine($"Hi,{name},What is your favorite color?");// output 
+    var color = Console.ReadLine(); // input
+
+    Console.WriteLine($"{color}, is great! Whats your favorite animal?");// output 
+    var animal = Console.ReadLine(); // input
+
+    Console.WriteLine($"{animal} is very cool! What is your favorite band?");// output 
+    var band = Console.ReadLine(); // input 
+}
